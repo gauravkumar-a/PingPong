ba18217d4b7dd756fc0d704932ba7a57a97bf1b8
AbradolfLinckler <gaurav19091@gmail.com>
2024-02-07 16:14:44 +0530

    First Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+
+
+export default function Home (){
+    return(
+        <div>
+            <h1>Hello World!</h1>
+            <button>Yes</button>
+            <button>No</button>
+        </div>
+    )
+}
