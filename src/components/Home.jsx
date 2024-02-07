40e01041c4cdb788f2fb765b88e45e268c366a85
AbradolfLinckler <gaurav19091@gmail.com>
2024-02-07 19:45:51 +0530

    setup gh-pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react"
-
+import "./home.css"
 
 export default function Home (){
+    const [buttonPosition, setButtonPosition] = useState({ x: window.innerWidth-100, y: window.innerHeight-100 });
+
+    const handleNoClick = () => {
+        const newX = Math.random() * (window.innerWidth - 100); // Adjust 100 according to button width
+        const newY = Math.random() * (window.innerHeight - 100); // Adjust 100 according to button height
+        setButtonPosition({ x: newX, y: newY });
+    };
+    const handleYesClick = () => {
+        window.alert("Thank you so much! I Love You!");
+    }
+
     return(
         <div>
-            <h1>Hello World!</h1>
-            <button>Yes</button>
-            <button>Noo!</button>
+            <h1>Will You be my Valentine?</h1>
+            <button className="button" onClick={handleYesClick}>Yes</button>
+            <button
+            style={{ position: "absolute", top: buttonPosition.y, left: buttonPosition.x }} 
+            onClick={handleNoClick}>
+                No
+            </button>
         </div>
     )
 }
